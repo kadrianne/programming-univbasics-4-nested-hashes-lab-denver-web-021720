08c4474174311c428ce1f01f45d5487af73957ab
Kristine Du <kristine.a.du@gmail.com>
2020-01-26 05:47:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-  programmer_hash.delete(:alan_kay,:dennis_ritchie)
+  programmer_hash.delete(:alan_kay)
+  programmer_hash.delete(:dennis_ritchie)
 
 end
 
